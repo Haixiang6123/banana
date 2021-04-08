0965a2ae30588dcabfd07e17c1b16e895e840e69
HaixiangYan <haixiang6123@gmail.com>
2021-04-09 01:02:31 +0800

    feat: 添加睡觉样式

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const $leftEye = document.querySelector('.left-eye')
+const $rightEye = document.querySelector('.right-eye')
 const $tongue = document.querySelector('.tongue')
 
 $tongue.addEventListener('click', () => {
-    document.body.classList.toggle('hover')
+    document.body.classList.toggle('wink')
+
+    $leftEye.classList.toggle('wink')
+    $rightEye.classList.toggle('wink')
 })
