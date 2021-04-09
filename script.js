f09862a61792be8519f9cc721c621f90ae468df1
HaixiangYan <haixiang6123@gmail.com>
2021-04-09 08:54:50 +0800

    feat: 点击 document.body

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const $leftEye = document.querySelector('.left-eye')
 const $rightEye = document.querySelector('.right-eye')
-const $tongue = document.querySelector('.tongue')
 
-$tongue.addEventListener('click', () => {
+document.body.addEventListener('click', () => {
     document.body.classList.toggle('wink')
 
     $leftEye.classList.toggle('wink')
